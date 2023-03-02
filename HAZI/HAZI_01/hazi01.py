ec2b77227dd29c2155f081d528ad1573c0c420e9
dezsi_csaba <dezsi_csaba@stud.uni-obuda.hu>
2023-03-02 14:50:53 +0100

    made some modifications here and there and removed test cases

--- HAZI/HAZI_01/hazi01.py
+++ HAZI/HAZI_01/hazi01.py
@@ -8,12 +8,13 @@
 # %%
 def subset(input_list, start_index, end_index):
     output = []
-    for i in range(len(input_list)):
-        if (i >= start_index and i < end_index):
-            output.append(input_list[i])
-    return output
+    ct = start_index
+    while (ct <= end_index):  #if we go with the assumption that the ending index is still part of the subset it's '<='
+        output.append(input_list[ct])       #if we don't wish to include the ending index then it is just '<'
+        ct += 1                             #I was unsure which way to go, so I went with the first
+    return output             #kinda same with the start index, but it is usually included before eincreasing the indexer
 
-subset([0,1,2,3,4], 1, 3)
+#subset([0,1,2,3,4], 0, 1)
 
 # %%
 #Create a function that returns every nth element of a list.
@@ -24,12 +25,13 @@ subset([0,1,2,3,4], 1, 3)
 # %%
 def every_nth(input_list,step_size):
     output = []
-    for i in range(len(input_list)):
-        if ((i+1) % step_size == 0):
-            output.append(input_list[i])
+    ct = step_size - 1  #unsure if we want the list[index] or the n-th
+    while (ct < len(input_list)):
+        output.append(input_list[ct])
+        ct += step_size    
     return output
 
-every_nth([0,1,2,3,4,5,6,7,8,9], 3)
+#every_nth([0,1,2,3,4,5,6,7,8,9], 2)
 
 # %%
 #Create a function that can decide whether a list contains unique values or not
@@ -45,7 +47,7 @@ def unique(input_list):
             return False
         else : out.append(item)
     return True
-unique([1,2,3,4,1])
+#unique([1,2,3,4,1])
 
 # %%
 #Create a function that can flatten a nested list ([[..],[..],..])
@@ -60,7 +62,7 @@ def flatten(input_list):
         for mini in nested:
             out.append(mini)
     return out
-flatten([[1,2],[1,2],[1,2]])
+#flatten([[1,2,3],[1,2],[1]])
 
 # %%
 #Create a function that concatenates n lists
@@ -76,7 +78,7 @@ def merge_list(*args):
         out += item
     return out
 
-merge_list([1,2],[3,4],[5,6])
+#merge_list([1,2],[[2,3],[4,5]],["a","b"],[{"a":"A", "b":"B"}])
 
 # %%
 #Create a function that can reverse a list of tuples
@@ -86,14 +88,14 @@ merge_list([1,2],[3,4],[5,6])
 #input parameters: input_list
 
 # %%
-def reverse_tuples(input_list):
+def reverse_tuples(input_list):  #this won't work with bigger tuples, but i assumed we need tuples with value pairs as in the exmaple
     out = []
     for tuple in input_list:
         newtuple = (tuple[1], tuple[0])
         out.append(newtuple)
     return out
 
-reverse_tuples([("x", "y"),("a", "b"),("b", "c")])
+#reverse_tuples([("x", "y"),("a", "b"),("b", "c")])
 
 # %%
 #Create a function that removes duplicates from a list
@@ -107,9 +109,10 @@ def remove_tuplicates(input_list):
     for item in input_list:
         if (item not in out):
             out.append(item)
-    return out
+    input_list = out
+    return input_list
 
-remove_tuplicates([1,2,3,4,5,2,3,4])
+#remove_tuplicates([1,2,3,4,5,2,3,4])
 
 # %%
 #Create a function that transposes a nested list (matrix)
@@ -126,12 +129,11 @@ def transpose(input_list):
         for j in range(len(input_list[i])):
             if len(out) < len(input_list[i]):
                 out.append([])
-
             out[j].append(input_list[i][j])
 
     return out
 
-transpose([[0,1],[2,3],[4,5]])
+#transpose([[0,1],[2,3],[4,5]])
 
 # %%
 #Create a function that can split a nested list into chunks
@@ -158,7 +160,7 @@ def split_into_chunks(input_list,chunk_size):
         out.append(chunk)
     return out
 
-split_into_chunks([[0,1],[2,3],[4,5]], 2)
+#split_into_chunks([[0,1],[2,3],[4,5]], 5)
 
 # %%
 #Create a function that can merge n dictionaries
@@ -174,13 +176,13 @@ def merge_dicts(*dict):
             out[x] = y
     return out
 
-merge_dicts({
-  "brand": "Ford",
-  "model": "Mustang",
-},{
-  "conference" : "Western",
-  "Division": "Pacific",
-})
+#merge_dicts({
+#  "brand": "Ford",
+#  "model": "Mustang",
+#},{
+#  "conference" : "Western",
+#  "Division": "Pacific",
+#})
 
 # %%
 #Create a function that receives a list of integers and sort them by parity
@@ -192,17 +194,13 @@ merge_dicts({
 # %%
 def by_parity(input_list):
     out={"even": [], "odd":[]}
-    even = []
-    odd = []
+
     for num in input_list:
-        if (num % 2 == 0):
-            even.append(num)
-        else: odd.append(num)
-    out["even"] = even
-    out["odd"] = odd
+        if (num % 2 == 0): out["even"].append(num)
+        else: out["odd"].append(num)    
     return out
 
-by_parity([1,2,3,4,5,6,7])
+#by_parity([1,2,3,4,5,6,7,7,2])
 
 # %%
 #Create a function that receives a dictionary like this: {"some_key":[1,2,3,4],"another_key":[1,2,3,4],....}
@@ -228,7 +226,7 @@ def mean_key_value(input_dict):
         out[key] = mean
     return out
 
-mean_key_value({"even": [2, 4, 6], "odd": [1, 3, 5, 7]})
+#mean_key_value({"even": [2, 4, 6, 8], "odd": [1, 3, 5, 7]})
 
 # %%
 #If all the functions are created convert this notebook into a .py file and push to your repo
