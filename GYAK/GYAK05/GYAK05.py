667715941b62b945cf9012b14160cfd30b871e3f
DezsiCsaba <csabeszq2001@gmail.com>
2023-04-05 10:17:42 +0200

    GYAK05 fixes, and train_test_split rename done

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 class KNNClassifier():
     def __init__(self, k:int, test_split_ratio:float):
@@ -8,37 +9,31 @@ class KNNClassifier():
         self.test_split_ratio = test_split_ratio
 
     @property
-    def k_neighbors(self) -> int:
+    def k_neighbors(self) -> int: #this is fine
         return self.k
 
-    def confusion_matrix(self) -> np.ndarray:
-        return confusion_matrix(self.y_test, self.y_preds)
-
     @staticmethod
-    def load_csv(cs_path : str):
+    def load_csv(cs_path : str): #this should be fine
         np.random.seed(42)
         dataset = np.genfromtxt(cs_path, delimiter=',')
         np.random.shuffle(dataset)
         x,y = dataset[:, :-1],dataset[:, -1]
         return x,y
 
-    def train_test_splitting(self, features: np.ndarray, labels: np.ndarray,):
-        
+    def train_test_split(self, features: np.ndarray, labels: np.ndarray,): #this should be fine
         test_size =int(len(features)*self.test_split_ratio)
         train_size = len(features) - test_size
-        assert len(features) ==test_size + train_size,"Size mismatch"
+        assert len(features) == test_size + train_size,"Size mismatch"
     
         self.x_train,self.y_train = features[:train_size,:],labels[:train_size]
         self.x_test,self.y_test = features[train_size:,:],labels[train_size:]
 
-    
-    
-    def euclidean(self, element_of_x: np.ndarray) -> np.ndarray:
+
+    def euclidean(self, element_of_x: np.ndarray) -> np.ndarray: #this should be fine
         return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
-    
 
 
-    def predict(self, x_test: np.ndarray):
+    def predict(self, x_test: np.ndarray): #this should be fine
         labels_pred = []
         for x_test_element in x_test:
             # Táv
@@ -46,12 +41,32 @@ class KNNClassifier():
             distances = np.array(sorted(zip(distances, self.y_train)))
             #Leggyakoribb value
             #dist móduszán
-            label_pred = mode(distances[:self.k, 1], keepdims =False).mode
+            label_pred = mode(distances[:self.k, 1], keepdims=False).mode
             labels_pred.append(label_pred)
-        self.y_preds = np.array(labels_pred, dtype=np.int64)
+
+        self.y_preds = np.array(labels_pred, dtype=np.int32)
 
 
-
-    def accuracy(self)->float:
+    def accuracy(self)->float: #this should be fine
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
+
+    def confusion_matrix(self) -> np.ndarray: #should be fixed
+        conf_matrix = confusion_matrix(self.y_test, self.y_preds)
+        sns.heatmap(conf_matrix,annot=True) 
+
+    
+
+    
+
+    
+    
+    
+    
+
+
+    
+
+
+
+    
