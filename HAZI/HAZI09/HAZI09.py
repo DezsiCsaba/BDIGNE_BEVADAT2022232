2471f22904efd0d45a8d2977276c6fe114f8766c
DezsiCsaba <csabeszq2001@gmail.com>
2023-04-26 12:15:21 +0200

    asd

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -1,4 +1,8 @@
 from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+import numpy as np
+
+from sklearn.decomposition import PCA
 
 class KMeansOnDigits():
     def __init__(self, n_clusters = 10, random_state = 42) -> None:
@@ -9,7 +13,24 @@ class KMeansOnDigits():
         self.digits = load_digits()
 
     def predict(self):
-        pass
+        self.model = KMeans(
+            n_clusters=self.n_clusters,
+            random_state=self.random_state,
+            init='random'
+        )
+
+        pca = PCA(2)
+        reduced = pca.fit_transform(self.digits)
+        print(reduced.shape)
+
+        X,y = self.digits.data, self.digits.target
+        labels = self.model.fit_predict(reduced)
+        unique_lab = np.unique(labels)
+
+        self.centroids = self.model.cluster_centers_
+
+        return self.centroids
+
 
     def get_labels(self):
         pass
