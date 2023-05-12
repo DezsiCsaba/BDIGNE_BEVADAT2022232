4fd169b03557a28335a31dd3f0a92d6192f475a9
Csaba Dezsi <csabeszq2001@gmail.com>
2023-05-12 20:01:31 +0200

    hazi11 will do for now

---
+++ HAZI/HAZI11/HAZI11.py
@@ -0,0 +1,99 @@
+# %%
+
+import tensorflow as tf
+import numpy as np
+
+'''
+Készíts egy metódust ami a cifar100 adatbázisból betölti a train és test adatokat. (tf.keras.datasets.cifar100.load_data())
+Majd a tanitó, és tesztelő adatokat normalizálja, és vissza is tér velük.
+
+
+Egy példa a kimenetre: train_images, train_labels, test_images, test_labels
+függvény neve: cifar100_data
+'''
+
+
+def cifar100_data():
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar100.load_data()
+    return train_images/255.0, train_labels/255.0, test_images/255.0, test_labels/255.0
+
+
+#train_images, train_labels, test_images, test_labels = cifar100_data()
+
+# %%
+'''
+Készíts egy konvolúciós neurális hálót, ami képes felismerni a képen mi van a 100 osztály közül.
+A háló kimenete legyen 100 elemű, és a softmax aktivációs függvényt használja.
+Hálon belül tetszőleges számú réteg lehet..
+
+Egy példa a kimenetre: model,
+return type: keras.engine.sequential.Sequential
+függvény neve: cifar100_model
+'''
+def cifar100_model() -> tf.keras.Sequential:
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64, activation='relu'))
+    model.add(tf.keras.layers.Dense(100, activation='softmax'))
+    return model
+
+#model = cifar100_model()
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálot compile-olja.
+Optimizer: Adam
+Loss: SparseCategoricalCrossentropy(from_logits=False)
+
+Egy példa a bemenetre: model
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_compile
+'''
+def model_compile(model) -> tf.keras.Sequential:
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+    return model
+
+
+#model_compile(model)
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálót feltanítja.
+
+Egy példa a bemenetre: model,epochs, train_images, train_labelsz
+Egy példa a kimenetre: model
+return type: keras.engine.sequential.Sequential
+függvény neve: model_fit
+'''
+def model_fit(model, epochs, train_images, train_labelsz) -> tf.keras.Sequential:
+    model = model.fit(
+        train_images,
+        train_labelsz,
+        epochs=epochs,
+        validation_data=(test_images, test_labels)
+        #validation_data = val_data
+        )
+    return model
+
+#model_fit(model, 10, train_images, train_labels)
+#model_fit(model, 10, train_images, train_labels, val_data=(test_images, test_labels))
+
+# %%
+'''
+Készíts egy metódust, ami a bemeneti hálót kiértékeli a teszt adatokon.
+
+Egy példa a bemenetre: model, test_images, test_labels
+Egy példa a kimenetre: test_loss, test_acc
+return type: float, float
+függvény neve: model_evaluate
+'''
+def model_evaluate(model, test_images, test_labels):
+    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+    return test_loss, test_acc
+
+#model_evaluate(model, test_images, test_labels)
+
+
